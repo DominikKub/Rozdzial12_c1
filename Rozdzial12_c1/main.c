e473e4d560de89ec41882149fb9e9b89e6beb25d
Dominik <kubickidom@icloud.com>
2022-07-25 18:01:07 +0200

    Initial Commit

---
+++ Rozdzial12_c1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Rozdzial12_c1
+//
+//  Created by Dominik on 25/07/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
